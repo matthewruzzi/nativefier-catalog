3647c2107c7b737cefe43ea62203f159ab10e1ab
Matthew Ruzzi <68619790+mattruzzi@users.noreply.github.com>
2021-06-05 15:42:02 -0700

    New schema PoC

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-var nativefier = require('nativefier').default;
+var nativefier = require("nativefier").default;
 const apps = require("./apps.json");
+const { MultiSelect } = require('enquirer');
 
 const createNativefierApp = (name = {}) => {
-	if (typeof name !== 'string') {
+	if (typeof name !== "string") {
 		throw new TypeError(`Expected a string, got ${typeof name}`);
 	}
+	if (apps[name].hasOwnProperty("additionalOptions")) {
+		var additionalOptionsList = Object.keys(apps[name].additionalOptions);
+		console.log(additionalOptionsList)
+		const prompt = new MultiSelect({  //TODO: Make options with "default": true selected by default
+			name: 'additionalOptions',
+			message: 'Available Additional Options ',
+			hint: '(Use <space> to select, <return> to submit)',
+			choices: additionalOptionsList
+		});
 
-	nativefier(apps[name], function (error, appPath) {
+		prompt.run()
+			.then(answer => runNativefier(getOptions(name, answer)))
+			.catch(console.error);
+	}
+	else {
+		var options = [];
+		options.name = apps[name].name;
+		options.targetUrl = apps[name].targetUrl;
+		runNativefier(options);
+	}
+}
+
+
+
+
+
+function getOptions(name = {}, selectedAdditionalOptions = {}) {
+	var options = [];
+	selectedAdditionalOptions.forEach(element => {
+		Object.keys(apps[name].additionalOptions[element].flags).forEach(option => {
+			options[option] = apps[name].additionalOptions[element].flags[option]
+		});
+	});
+	options.name = apps[name].name;
+	options.targetUrl = apps[name].targetUrl;
+	console.dir(options);
+	return options;
+}
+
+/*runNativefier(config);*/
+//return `${name} & ${postfix}`;
+
+function runNativefier(config = {}) {
+	nativefier(config, function (error, appPath) {
 		if (error) {
 			console.error(error);
 			return;
 		}
-		console.log('App has been nativefied to', appPath);
+		console.log("App has been nativefied to", appPath);
 	});
-	//return `${name} & ${postfix}`;
-};
-
+}
 module.exports = createNativefierApp;
